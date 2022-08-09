c0a0f7f1a68d2280e218e6dfe97c1bc67dbddaa6
Vadym8177 <vadim.8177@icloud.com>
2022-08-09 17:35:00 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector(['[data-start]']);
+const stoptBtn = document.querySelector(['[data-stop]']);
+let timerId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stoptBtn.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+  timerId = setInterval(changeBgColor, 1000);
+
+  startBtn.disabled = true;
+  stoptBtn.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+
+  stoptBtn.disabled = true;
+  startBtn.disabled = false;
+}
+
+function changeBgColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+}
