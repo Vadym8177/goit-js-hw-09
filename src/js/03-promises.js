20c611d338394a79aca95b96a110d54cba06e801
Vadym8177 <vadim.8177@icloud.com>
2022-08-11 22:03:19 +0300

    settings

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+const form = document.querySelector('.form');
+
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(form.delay.value);
+
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
