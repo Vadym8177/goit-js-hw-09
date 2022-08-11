554bd3cf410fba236ed167cb6e9fb1a622a0ffb4
Vadym8177 <104273571+Vadym8177@users.noreply.github.com>
2022-08-11 19:03:47 +0000

    Deploying to gh-pages from @ Vadym8177/goit-js-hw-09@20c611d338394a79aca95b96a110d54cba06e801 🚀

---
+++ 03-promises.e39753b5.js
@@ -0,0 +1,2 @@
+var form=document.querySelector(".form");function onFormSubmit(o){o.preventDefault(),console.log(form.delay.value)}function createPromise(o,e){Math.random()}form.addEventListener("submit",onFormSubmit);
+//# sourceMappingURL=03-promises.e39753b5.js.map
